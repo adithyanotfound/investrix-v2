654222d5568e77e1bb6f4b59b485c83d2555ee66
adithya <adithyav102@gmail.com>
2025-02-28 05:57:40 +0530

    Fix eslint

--- src/app/dashboard/investee/bids/page.tsx
+++ src/app/dashboard/investee/bids/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-/* eslint disable */
+/* eslint-disable */
 import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Button } from "@/components/ui/button";
